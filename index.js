394e8ca2f1c4345194fa4a2f6c7c2bc3020e7e9a
deashawnogle <deashawnogle@gmail.com>
2022-02-05 20:05:28 -0800

    edit index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
 const fs = require("fs");
 const inquirer = require("inquirer");
+const path = require('path');
 const Intern = require("./Develop/lib/Intern");
 const Manager = require("./Develop/lib/Manager");
 const Engineer = require("./Develop/lib/Engineer");
+const render = require("./Develop/lib/htmlRenderer");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "profile.html");
+const employee = [];
 
 const questions = [{
     type: "input",
@@ -30,7 +35,107 @@ const questions = [{
         "Engineer"
     ]
 }
-]
+];
+
+
+function employeeQuestion() {
+    inquirer.prompt(questions)
+    .then(response => {
+        let employeeRole = response.role;
+
+        if (employeeRole === "Manager") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is your Office Number?",
+                    name: "officeNumber"
+                },
+                {
+                    type: "list",
+                    message: "Add more Team Members?",
+                    name: "moreEmployee",
+                    choices: [
+                        "Yes",
+                        "No"
+                    ]
+                }
+            ]).then(function(employeeInfo) {
+                let newMember = new Manager (response.name, response.id, response.email, employeeInfo.officeNumber);
+                employee.push(newMember);
+                if (employee.moreEmployee === "Yes") {
+                    employeeQuestion();
+                }
+                else {
+                    createHTML();
+                    console.log("HTML Created!!!");
+                }
+            })
+        }
+        else if (employeeRole === "Engineer") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is Github ID?",
+                    name: "github",
+                },
+                {
+                    type: "list",
+                    message: "Add more Team Members?",
+                    name: "moreEmployee",
+                    choices: [
+                        "Yes",
+                        "No"
+                    ]
+                }
+            ]).then(function(employeeInfo) {
+                let newMember = new Engineer(response.name, response.id, response.email, employeeInfo.github);
+                employee.push(newMember);
+                if (employeeInfo.moreEmployee === "Yes") {
+                    employeeQuestion ();
+                }
+                else {
+                    createHTML();
+                    console.log("HTML Created!!!");
+                }
+            })
+        }
+        else if (employeeRole === "Intern") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Which School attended?",
+                    name : "school"
+                },
+                {
+                    type: "list",
+                    message: "Add more Team Members?",
+                    name: "moreEmployee",
+                    choices: [
+                        "Yes",
+                        "No"
+                    ]
+                }
+            ]).then(function(employeeInfo) {
+                let newMember = new Intern(response.name, response.id, response.email, employeeInfo.school);
+                employee.push(newMember);
+                if (employeeInfo.moreEmployee === "Yes") {
+                    employeeQuestion();
+                }
+                else {
+                    createHTML();
+                    console.log("HTML Created!!!");
+
+                }
+            })
+        }
+    })
+}
+
+employeeQuestion();
+
+function createHTML() {
+    fs.writeFileSync(outputPath, render(employee), "utf-8")
+}
 
 
 
